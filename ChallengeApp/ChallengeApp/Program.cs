7823b6f533cbceb7c1e4904d34dc2b87c5cd89d7
ijot1 <ireneuszjaneczek128@gmail.com>
2023-02-16 23:07:06 +0100

    variables types & if excercise

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name;
+int age;
+int limit = 30;
+char sex;
+
+name = "Greta";
+age = 28;
+sex = 'F';
+if (sex == 'F' && age < limit)
+{
+    Console.WriteLine("\"" + "Kobieta poniżej " + limit + " lat" + "\"");
+}
+
+name = "Ewa";
+age = 33;
+sex = 'F';
+if (name == "Ewa" && sex == 'F')
+{ 
+    Console.WriteLine("\"" + name + ", lat " + age + "\"");
+}
+
+
+name = "Adam";
+age = 17;
+sex = 'M';
+if (sex =='M' && age < 18)
+{
+    Console.WriteLine("\"" + "Niepełnoletni mężczyzna" + "\"");
+}
+
